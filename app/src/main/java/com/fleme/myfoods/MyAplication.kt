b05f04cef437e96165b9197bcac59c4bb75d163d
Felipe Leme <lemefe21@gmail.com>
2021-01-22 11:27:43 -0300

    Change gitignore and remove unused import

--- app/src/main/java/com/fleme/myfoods/MyAplication.kt
+++ app/src/main/java/com/fleme/myfoods/MyAplication.kt
@@ -3,7 +3,6 @@ package com.fleme.myfoods
 import android.app.Application
 import com.fleme.myfoods.di.FoodModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class MyAplication: Application() {
